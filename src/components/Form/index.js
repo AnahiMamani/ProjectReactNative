0c63ab80b6b5559b865ef1ef5d91d54c8788672b
AnahiMamani <narielanahi@gmail.com>
2023-07-13 10:29:41 -0300

    first commit

---
+++ src/components/Form/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import {View, Text, TextInput, Button} from "react-native"
+
+export default function Form(){
+    return(
+        <View>
+            <View>
+             <Text>Altura</Text>
+             <TextInput 
+             placeholder="Ex. 1.75" 
+             keyboardType="numeric"/>
+             <Text>Peso</Text>
+             <TextInput 
+             placeholder="Ex. 75.365" 
+             keyboardType="numeric"/>
+             <Button title="Calcular IMC"></Button>
+            </View>
+            <ResultImc messageResultImc={mesageImc} resultImc={imc}/>
+        </View>
+    );
+}
